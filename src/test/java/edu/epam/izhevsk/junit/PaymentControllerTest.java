9ebb26e547c0e1862886d7b8f81a6d7a548f52db
Alexander@work <tl@epam.com>
2015-02-26 16:37:56 +0400

    added test file

---
+++ src/test/java/edu/epam/izhevsk/junit/PaymentControllerTest.java
@@ -0,0 +1,22 @@
+package edu.epam.izhevsk.junit;
+
+import org.hamcrest.CustomMatcher;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import static org.mockito.AdditionalMatchers.*;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import static org.mockito.Mockito.*;
+
+
+
+
+public class PaymentControllerTest {
+
+ 
+}
