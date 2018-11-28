9346a6b5534af4a1055cd805db88e452c140df1a
Petr Bunegin <bunegin.p@gmail.com>
2018-11-28 17:00:38 +0400

    mockito

--- src/test/java/edu/epam/izhevsk/junit/PaymentControllerTest.java
+++ src/test/java/edu/epam/izhevsk/junit/PaymentControllerTest.java
@@ -23,20 +23,24 @@ public class PaymentControllerTest {
     @BeforeEach
     void testInitialMock(){
         MockitoAnnotations.initMocks(this);
+        when(accountService.isUserAuthenticated(100L)).thenReturn(true);
+        assertDoesNotThrow(()->when(depositService.deposit(lt(100L), any())).thenReturn("successfully"));
+        assertDoesNotThrow(()->when(depositService.deposit(geq(100L), any())).thenThrow(InsufficientFundsException.class));
     }
 
     @Test
-    void testAccountAndDepositService() {
-
-        when(accountService.isUserAuthenticated(100L)).thenReturn(true);
-
-        assertDoesNotThrow(()->when(depositService.deposit(lt(100L), any())).thenReturn("successfully"));
-        assertDoesNotThrow(()->when(depositService.deposit(geq(100L), any())).thenThrow(InsufficientFundsException.class));
-
+    void testSuccessfulDeposit(){
         assertDoesNotThrow(()->paymentController.deposit(50L,100L));
         verify(accountService,times(1)).isUserAuthenticated(100L);
+    }
 
-        assertThrows(Exception.class,()->paymentController.deposit(500L,10L));
+    @Test
+    void testFailedDepositForUnauthenticatedUser(){
+        assertThrows(SecurityException.class,()->paymentController.deposit(500L,10L));
+    }
+
+    @Test
+    void failedDepositOfLargeAmount(){
         assertThrows(InsufficientFundsException.class,()->paymentController.deposit(500L,100L));
     }
 }
