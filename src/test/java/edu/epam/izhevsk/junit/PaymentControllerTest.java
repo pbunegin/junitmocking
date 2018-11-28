8c478ab7f88e529ed01ddf987a9715964ed52918
Petr Bunegin <bunegin.p@gmail.com>
2018-11-28 16:18:34 +0400

    mockito

--- src/test/java/edu/epam/izhevsk/junit/PaymentControllerTest.java
+++ src/test/java/edu/epam/izhevsk/junit/PaymentControllerTest.java
@@ -1,22 +1,42 @@
 package edu.epam.izhevsk.junit;
 
-import org.hamcrest.CustomMatcher;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import static org.mockito.AdditionalMatchers.*;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.AdditionalMatchers.*;
 import static org.mockito.Mockito.*;
 
 
-
-
 public class PaymentControllerTest {
+    @Mock
+    AccountService accountService;
+    @Mock
+    DepositService depositService;
+    @InjectMocks
+    PaymentController paymentController;
 
- 
+    @Test
+    @BeforeEach
+    void testInitialMock(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void testAccountAndDepositService() {
+
+        when(accountService.isUserAuthenticated(100L)).thenReturn(true);
+
+        assertDoesNotThrow(()->when(depositService.deposit(lt(100L), any())).thenReturn("successfully"));
+        assertDoesNotThrow(()->when(depositService.deposit(geq(100L), any())).thenThrow(InsufficientFundsException.class));
+
+        assertDoesNotThrow(()->paymentController.deposit(50L,100L));
+        verify(accountService,times(1)).isUserAuthenticated(100L);
+
+        assertThrows(SecurityException.class,()->paymentController.deposit(500L,10L));
+        assertThrows(Exception.class,()->paymentController.deposit(500L,100L));
+    }
 }
